32f61920d4a54ebc732c4d92c7d7fc94a41cbcac
MAJID NISAR <majidnisar@gmail.com>
2021-05-12 14:40:06 +0530

    introduced our own router or request multiplexer

--- app/app.go
+++ app/app.go
@@ -11,12 +11,16 @@ func Start() {
 	// 	fmt.Println("Greet Request hit")
 	// 	fmt.Fprint(rw, "Hello World")
 	// })
+	// we can define our own request multiplexer or router
+	router := http.NewServeMux()
 
-	// defining the routes
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customers", getAllCustomers)
+	// defining the routes using router and not http
+	// http.HandleFunc("/greet", greet) it was done previously like this
+	router.HandleFunc("/greet", greet)
+	router.HandleFunc("/customers", getAllCustomers)
 
 	// starting the server and listeing on the port and log.fatal in case of error starting the server will be logged
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	// log.Fatal(http.ListenAndServe("localhost:8000", nil)) default multiplexer or router
+	log.Fatal(http.ListenAndServe("localhost:8000", router))
 
 }
